b0bade6292dcea959e1892a839de4408bcd4dd58
Thomas Schraven <t.schraven@student.fontys.nl>
2014-09-27 21:49:28 +0200

    removed stupid default documentation

--- TowerHunterEngine/Engine.cs
+++ TowerHunterEngine/Engine.cs
@@ -9,9 +9,6 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TowerHunterEngine
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Engine : Game
     {
         private readonly Point GAMERES = new Point(1024, 600);
@@ -40,45 +37,23 @@ namespace TowerHunterEngine
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             SetupPlayfield(GAMERES, FIELDSIZE, TOWERS, ref playField);
 
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             //font = Content.Load<SpriteFont>("default")
         }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             KeyboardState newState = Keyboard.GetState();
@@ -99,10 +74,6 @@ namespace TowerHunterEngine
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
