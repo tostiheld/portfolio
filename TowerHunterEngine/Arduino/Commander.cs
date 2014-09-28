6e5ab48c63961f16ae0f2e38b6cb494d8de9bb04
Thomas Schraven <t.schraven@student.fontys.nl>
2014-09-28 23:04:30 +0200

    added basic methods to communicate with an Arduino board

--- TowerHunterEngine/Arduino/Commander.cs
+++ TowerHunterEngine/Arduino/Commander.cs
@@ -1,17 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using CommandMessenger;
+using CommandMessenger.TransportLayer;
 
 namespace TowerHunterEngine.Arduino
 {
-    public class Commander
+    public class Commander : IDisposable
     {
-        public Commander()
+        private SerialTransport Transport;
+        private CmdMessenger Messenger;
+
+        public Commander(String portname, int baudrate, BoardType boardtype)
         {
-            
+            Transport = new SerialTransport
+            {
+                CurrentSerialSettings =
+                { 
+                    PortName = portname, 
+                    BaudRate = baudrate, 
+                    DtrEnable = false
+                }
+            };
+
+            Messenger = new CmdMessenger(Transport)
+            {
+                BoardType = boardtype
+            };
+
+            AttachCommandCallBacks();
+            Messenger.NewLineReceived += Messenger_NewLineReceived;
+            Messenger.NewLineSent += Messenger_NewLineSent;
+            Messenger.StartListening();
+        }
+
+        private void Messenger_NewLineSent(object sender, NewLineEvent.NewLineArgs e)
+        {
+            // TODO: handle event
+            throw new NotImplementedException();
+        }
+
+        private void Messenger_NewLineReceived(object sender, NewLineEvent.NewLineArgs e)
+        {
+            // TODO: handle event
+            throw new NotImplementedException();
+        }
+
+        private void AttachCommandCallBacks()
+        {
+            // TODO: attach commands to methods
+            throw new NotImplementedException();
+        }
+
+        public TowerState GetTowerState()
+        {
+            TowerState state = new TowerState();
+            // TODO: receive and store state of the towers
+            return state;
+        }
+
+        public bool SetTowerState(TowerState state)
+        {
+            // TODO: set state of the towers
+            return true;
+        }
+
+        public void Stop()
+        {
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Messenger.StopListening();
+                Messenger.Dispose();
+                Transport.Dispose();
+            }
         }
     }
 }
