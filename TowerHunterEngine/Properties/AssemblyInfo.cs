9d50a88a2a5260d50fcb40b6c65cdcb0059c54b5
Thomas Schraven <t.schraven@student.fontys.nl>
2014-09-29 12:03:20 +0200

    added console library

--- TowerHunterEngine/Properties/AssemblyInfo.cs
+++ TowerHunterEngine/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2014.9.29.24")]
-[assembly: AssemblyFileVersion("2014.9.29.24")]
+[assembly: AssemblyVersion("2014.9.29.25")]
+[assembly: AssemblyFileVersion("2014.9.29.25")]
