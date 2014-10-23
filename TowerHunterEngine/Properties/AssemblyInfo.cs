9ed727f43f6d2278db8bbfd04be4193b5c3d17e7
Thomas Schraven <t.schraven@student.fontys.nl>
2014-10-23 15:29:47 +0200

    Recovered CommandMessenger library

--- TowerHunterEngine/Properties/AssemblyInfo.cs
+++ TowerHunterEngine/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2014.10.23.56")]
-[assembly: AssemblyFileVersion("2014.10.23.56")]
+[assembly: AssemblyVersion("2014.10.23.57")]
+[assembly: AssemblyFileVersion("2014.10.23.57")]
