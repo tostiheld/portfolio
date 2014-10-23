1af3d395bf532a63e8579a950cf56fb39e5ef3e8
Thomas Schraven <t.schraven@student.fontys.nl>
2014-10-23 15:24:04 +0200

    Added PlayerInput code

--- TowerHunterEngine/Player/PlayerInput.cs
+++ TowerHunterEngine/Player/PlayerInput.cs
@@ -3,9 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
 namespace TowerHunterEngine.Player
 {
-    public class PlayerInput
+    public static class PlayerInput
     {
+        public static Point GetDirections(int scale, int correctionScale)
+        {
+            GamePadState state = GamePad.GetState(PlayerIndex.One);
+
+            float stateX = state.ThumbSticks.Left.X;
+            float stateY = state.ThumbSticks.Left.Y;
+
+            int baseSpeed = (int)(stateY * scale);
+            int correction = Math.Abs((int)(stateX * correctionScale));
+
+            int leftSpeed = baseSpeed;
+            int rightSpeed = baseSpeed;
+
+            if (stateX > 0)
+            {
+                leftSpeed += correction;
+                rightSpeed -= correction;
+            }
+            else if (stateX < 0)
+            {
+                leftSpeed -= correction;
+                rightSpeed += correction;
+            }
+
+            Point p = new Point(leftSpeed, rightSpeed);
+            return p;
+        }
     }
 }
