5a3b9fba60018c04c9adcd736d7a4e780e35f3f2
Thomas Schraven <t.schraven@student.fontys.nl>
2014-11-02 21:34:18 +0100

    added GameOverView class

---
+++ TowerHunterEngine/PlayerFeedback/GameOverView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace BombDefuserEngine.PlayerFeedback
+{
+    public class GameOverView : DrawableGameComponent
+    {
+        public GameOverView(Game game) : base(game)
+        {
+
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
