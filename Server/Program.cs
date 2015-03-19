652718ee1ae5938d22ae0187fab198a6448285f7
Thomas Schraven <t.schraven@student.fontys.nl>
2015-03-19 12:34:31 +0100

    graphicsdevice is broken

---
+++ Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Server.Simulation;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (Engine e = new Engine())
+            {
+                e.Run();
+            }
+        }
+    }
+}
---
+++ Server/Simulation/Engine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Server.Simulation
+{
+    public class Engine : Game
+    {
+        GraphicsDevice graphics;
+        SpriteBatch spriteBatch;
+
+        public Engine()
+            : base()
+        {
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            GraphicsDeviceManager graphicsd = new GraphicsDeviceManager(this);
+            graphicsd.CreateDevice();
+            graphics = graphicsd.GraphicsDevice;
+            spriteBatch = new SpriteBatch(graphics);
+
+            Content.RootDirectory = "";
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.Clear(Color.Green);
+
+            base.Draw(gameTime);
+
+            spriteBatch.Begin();
+
+            spriteBatch.End();
+        }
+    }
+}
