a9de323c7188cb6082fa87e196f41c3413c28abd
Wout van den Dungen <woutvandendungen@outlook.com>
2015-03-24 16:05:02 +0100

    Added Arduino Servo Test

---
+++ Arduino Files/Sonar/Servo_Test/Servo_Test.ino
@@ -0,0 +1,25 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+ 
+void loop() 
+{                           
+    myservo.write(180);
+    delay(2000);
+    myservo.write(0);
+    delay(2000);
+}
