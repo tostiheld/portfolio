13d65699a34abea985a5e6bfcd26a6630103beec
Thomas Schraven <t.schraven@student.fontys.nl>
2015-04-15 00:58:21 +0200

    new timerange class

--- RoadplusServer/Utils/TimeRange.cs
+++ RoadplusServer/Utils/TimeRange.cs
@@ -1,46 +1,36 @@
 ﻿using System;
 
-namespace Roadplus.Server.Utils
+namespace Roadplus.Server
 {
 	public class TimeRange
 	{
-		public TimeSpan BeginTime { get; private set; }
-		public TimeSpan EndTime { get; private set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+		
+        public TimeRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
 
-		public TimeRange (TimeSpan beginTime, TimeSpan endTime)
-		{
-			if (beginTime > endTime) {
-				throw new ArgumentException ("beginTime is later than endTime");
-			}
-			else if (beginTime == TimeSpan.Zero) {
-				throw new ArgumentException ("beginTime is not set");
-			}
-			else if (endTime == TimeSpan.Zero) {
-				throw new ArgumentException ("endTime is not set");
-			}
-			else if (beginTime < TimeSpan.Zero) {
-				throw new ArgumentOutOfRangeException ("beginTime");
-			}
-			else if (endTime < TimeSpan.Zero) {
-				throw new ArgumentOutOfRangeException ("endTime");
-			}
+        public TimeRange(DateTime end)
+            : this(DateTime.Now, end)
+        { }
 
-			BeginTime = beginTime;
-			EndTime = endTime;
-		}
+        public bool IsInRange(DateTime datetime)
+        {
+            if (datetime >= Start &&
+                datetime <= End)
+            {
+                return true;
+            }
 
-		public Boolean IsActive(DateTime date){
-			TimeSpan currentTime = date.TimeOfDay;
-			if (currentTime > BeginTime && currentTime < EndTime) {
-				return true;
-			}
-			return false;
-		}
+            return false;
+        }
 
-		public Boolean IsActive(){
-			return IsActive (DateTime.Now);
-		}
-			
-	}
+        public bool IsInRange()
+        {
+            return IsInRange(DateTime.Now);
+        }
+    }
 }
-
