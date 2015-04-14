42ec663d5028600364d9e7e64f4e044fd989bb57
Stijn Buurman <s.buurman@student.fontys.nl>
2015-04-14 16:03:32 +0200

    Added TimeRange and School class

--- RoadplusServer/Map/School.cs
+++ RoadplusServer/Map/School.cs
@@ -4,8 +4,19 @@ namespace Roadplus.Server.Map
 {
     public class School
     {
-        public School()
+		public TimeRange TimeRange { get; private set; }
+		public String Name { get; private set; }
+
+		public School(TimeRange timeRange, String name)
         {
+			if (timeRange == null) {
+				throw new ArgumentNullException ("timeRange");
+			} else if (name == null) {
+				throw new ArgumentNullException ("name");
+			} 
+
+			TimeRange = timeRange;
+			Name = name;
         }
     }
 }
---
+++ RoadplusServer/Map/TimeRange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Roadplus.Server.Map
+{
+	public class TimeRange
+	{
+		public TimeSpan BeginTime { get; private set; }
+		public TimeSpan EndTime { get; private set; }
+
+		public TimeRange (TimeSpan beginTime, TimeSpan endTime)
+		{
+			if (beginTime > endTime) {
+				throw new ArgumentException ("beginTime is later than endTime");
+			}
+			if (beginTime == TimeSpan.Zero || endTime == TimeSpan.Zero) {
+				throw new ArgumentNullException ();
+			}
+			if (beginTime < TimeSpan.Zero || endTime < TimeSpan.Zero) {
+				throw new ArgumentOutOfRangeException ();
+			}
+
+			BeginTime = beginTime;
+			EndTime = endTime;
+		}
+
+		public Boolean IsActive(DateTime date = DateTime.Now){
+			TimeSpan currentTime = date.Now.TimeOfDay;
+			if (currentTime > BeginTime && currentTime < EndTime) {
+				return true;
+			}
+			return false;
+		}
+			
+	}
+}
+
