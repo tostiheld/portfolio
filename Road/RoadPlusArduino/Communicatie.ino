b539d70912607e4257bafeed6624b55d352ab6e2
Nick van den Heuvel <nickvdheuvel@hotmail.com>
2015-04-17 13:36:35 +0200

    De meest geakse borden
    mede mogelijk gemaakt door asus

--- Road/RoadPlusArduino/Communicatie.ino
+++ Road/RoadPlusArduino/Communicatie.ino
@@ -9,19 +9,41 @@ void GetMessage()
   int endSymbol = message.indexOf(';');
   int metaSymbol = message.indexOf(':');
   if (startSymbol > -1 && endSymbol > -1 && metaSymbol > -1)
-  { 
-    meta = message.substring(startSymbol + 1, metaSymbol); //xxxx
-    metaMessage = message.substring(metaSymbol + 1, endSymbol); //iets
-    if (metaMessage.indexOf(':') > -1)
+  {
+    meta = message.substring(startSymbol + 1, metaSymbol);
+
+    if (meta == "TEMP" || meta == "SIGN" || meta == "DIST")
     {
-      metaMessage = metaMessage.substring(0, metaMessage.indexOf(':'));
+      metaMessage = message.substring(metaSymbol + 1, endSymbol);
+      if (metaMessage.indexOf(':') > -1)
+      {
+        metaMessage = metaMessage.substring(0, metaMessage.indexOf(':'));
+        Serial.println(metaMessage);
+      }
+      message = "";
     }
-    message = "";
+    else
+    {
+      Serial.println("<Invalid meta;");
+      message = "";
+    }
   }
 }
 
 void GetSpeedLimit()
 {
+  if (metaMessage == "Warning")
+  {
+    warning = true;
+  }  
+  if (metaMessage == "ebola")  //verander variable naam
+  {
+    warning = false;
+  }
+  if (metaMessage == "1way")
+  {
+      Display(OneDirection);
+  }
   if (metaMessage == "10")
   {
     speedLimit = 10;
