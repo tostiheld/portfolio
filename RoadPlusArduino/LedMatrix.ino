5b348cd44d5885a18ed1c8edfe6df319205ea1b9
Nick van den Heuvel <nickvdheuvel@hotmail.com>
2015-04-17 13:36:35 +0200

    De meest geakse borden
    mede mogelijk gemaakt door asus

--- RoadPlusArduino/LedMatrix.ino
+++ RoadPlusArduino/LedMatrix.ino
@@ -2,29 +2,74 @@
 void DisplaySpeedLimit()
 {
   GetSpeedLimit();
-  switch (speedLimit)
+  if (warning)
   {
-    case 10: Display(sign10); break;
-    case 15: Display(sign15); break;
-    case 20: Display(sign20); break;
-    case 25: Display(sign25); break;
-    case 30: Display(sign30); break;
-    case 35: Display(sign35); break;
-    case 40: Display(sign40); break;
-    case 45: Display(sign45); break;
-    case 50: Display(sign50); break;
-    case 55: Display(sign55); break;
-    case 60: Display(sign60); break;
-    case 65: Display(sign65); break;
-    case 70: Display(sign70); break;
-    case 75: Display(sign75); break;
-    case 80: Display(sign80); break;
-    case 85: Display(sign85); break;
-    case 90: Display(sign90); break;
-    case 95: Display(sign95); break;
-    case 100: Display(sign100); break;
-    case 110: Display(sign110); break;
-    case 120: Display(sign120); break;
+    timetillnext = millis();
+    if (timetillnext - previousTime > 1000)
+    {
+      previousTime = timetillnext;
+      state = !state;
+    }
+    if (state)
+    {
+      Display(signWarning);
+    }
+    else if (!state)
+    {
+      switch (speedLimit)
+      {
+        case 10: Display(sign10); break;
+        case 15: Display(sign15); break;
+        case 20: Display(sign20); break;
+        case 25: Display(sign25); break;
+        case 30: Display(sign30); break;
+        case 35: Display(sign35); break;
+        case 40: Display(sign40); break;
+        case 45: Display(sign45); break;
+        case 50: Display(sign50); break;
+        case 55: Display(sign55); break;
+        case 60: Display(sign60); break;
+        case 65: Display(sign65); break;
+        case 70: Display(sign70); break;
+        case 75: Display(sign75); break;
+        case 80: Display(sign80); break;
+        case 85: Display(sign85); break;
+        case 90: Display(sign90); break;
+        case 95: Display(sign95); break;
+        case 100: Display(sign100); break;
+        case 110: Display(sign110); break;
+        case 120: Display(sign120); break;
+        default: Display(Init_Display); break;
+      }
+    }
+
+  }
+  else if (!warning)
+  {
+    switch (speedLimit)
+    {
+      case 10: Display(sign10); break;
+      case 15: Display(sign15); break;
+      case 20: Display(sign20); break;
+      case 25: Display(sign25); break;
+      case 30: Display(sign30); break;
+      case 35: Display(sign35); break;
+      case 40: Display(sign40); break;
+      case 45: Display(sign45); break;
+      case 50: Display(sign50); break;
+      case 55: Display(sign55); break;
+      case 60: Display(sign60); break;
+      case 65: Display(sign65); break;
+      case 70: Display(sign70); break;
+      case 75: Display(sign75); break;
+      case 80: Display(sign80); break;
+      case 85: Display(sign85); break;
+      case 90: Display(sign90); break;
+      case 95: Display(sign95); break;
+      case 100: Display(sign100); break;
+      case 110: Display(sign110); break;
+      case 120: Display(sign120); break;
+    }
   }
 }
 
