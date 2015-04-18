48c32b87a4c0c7ea6fe16ee5da82d81a41721ded
Stijn Buurman <s.buurman@student.fontys.nl>
2015-04-18 11:00:33 +0200

    last update before move to knockoutjs

--- assets/js/Models/Zone.js
+++ assets/js/Models/Zone.js
@@ -7,11 +7,20 @@ function Zone(id, name) {
     this.RoadConstructionList = new RoadConstructions($("#roadConstructionTable"));
     this.VertexList = [];
     this.EdgeList = [];
+    this.arduinoPort = "";
 }
 
 function Zones(table) {
     this.ZoneList = [];
     this.Element = table;
+}
+
+Zones.prototype.byID = function (zoneID) {
+    for (var key in this.ZoneList) {
+        if (this.ZoneList[key].ID == id) {
+            return this.ZoneList[key];
+        }
+    }
 }
 
 Zones.prototype.Add = function (newZone) {
@@ -21,10 +30,13 @@ Zones.prototype.Add = function (newZone) {
     this.addToGUI(newZone);
 }
 Zones.prototype.addToGUI = function (newZone) {
-    $("> tbody", this.Element).append('<tr id="' + newZone.ID + '"><td>' + newZone.ID + '</td><td>' + newZone.Name + '</td><td> ' + newZone.SchoolList.SchoolList.length + ' </td><td>' + newZone.RoadConstructionList.RoadConstructionList.length + '</td><td>' + newZone.VertexList.length + '</td><td>' + newZone.EdgeList.length + '</td><td><span class="remove glyphicon glyphicon-remove" aria-hidden="true"></span></td></tr>');
+    $("> tbody", this.Element).append('<tr id="' + newZone.ID + '"><td>' + newZone.ID + '</td><td>' + newZone.Name + '</td><td> ' + newZone.SchoolList.SchoolList.length + ' </td><td>' + newZone.RoadConstructionList.RoadConstructionList.length + '</td><td>' + newZone.VertexList.length + '</td><td>' + newZone.EdgeList.length + '</td><td><span class="remove glyphicon glyphicon-remove" aria-hidden="true"></span><span class="addArduino glyphicon glyphicon-king" aria-hidden="true"></span></td></tr>');
 }
 Zones.prototype.updateGUI = function (zone) {
-    $("> tbody > tr#" + zone.ID, this.Element).html('<td>' + zone.ID + '</td><td>' + zone.Name + '</td><td> ' + zone.SchoolList.SchoolList.length + ' </td><td>' + zone.RoadConstructionList.RoadConstructionList.length + '</td><td>' + zone.VertexList.length + '</td><td>' + zone.EdgeList.length + '</td><td><span class="remove glyphicon glyphicon-remove" aria-hidden="true"></span></td>');
+    $("> tbody > tr#" + zone.ID, this.Element).html('<td>' + zone.ID + '</td><td>' + zone.Name + '</td><td> ' + zone.SchoolList.SchoolList.length + ' </td><td>' + zone.RoadConstructionList.RoadConstructionList.length + '</td><td>' + zone.VertexList.length + '</td><td>' + zone.EdgeList.length + '</td><td><span class="remove glyphicon glyphicon-remove" aria-hidden="true"></span><span class="addArduino glyphicon glyphicon-king" aria-hidden="true"></span></td>');
+    if (zone.arduinoPort.length > 0) {
+        $("tr#" + zone.ID + " .addArduino").css("color", "green");
+    }
 }
 Zones.prototype.Remove = function (id) {
     for (var key in this.ZoneList) {
