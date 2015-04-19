00efbadd21e3b54eb76ed729dae1048873561d0c
Thomas Schraven <t.schraven@student.fontys.nl>
2015-04-20 00:33:46 +0200

    implemented autoincrementingdictionary

--- Roadplus.Server/Utilities/AutoIncrementingDictionary.cs
+++ Roadplus.Server/Utilities/AutoIncrementingDictionary.cs
@@ -1,11 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 namespace Roadplus.Server
 {
-    public class AutoIncrementingDictionary
+    public class AutoIncrementingDictionary<T>
     {
+        private Dictionary<int, T> internalDictionary;
+        private int index;
+
         public AutoIncrementingDictionary()
         {
+            internalDictionary = new Dictionary<int, T>();
+            index = 0;
+        }
+
+        public void Add(T item)
+        {
+            internalDictionary.Add(index, item);
+            index++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            internalDictionary.Remove(index);
+        }
+
+        public bool TryGetValue(int index, out T value)
+        {
+            return internalDictionary.TryGetValue(index, out value);
         }
     }
 }
