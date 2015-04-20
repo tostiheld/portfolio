4e5abe1929af9046c3a49608970dd3dec9d10a4e
Thomas Schraven <t.schraven@student.fontys.nl>
2015-04-20 23:18:31 +0200

    made activitytype serializable

--- Roadplus.Server/EntityManagement/ActivityType.cs
+++ Roadplus.Server/EntityManagement/ActivityType.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Roadplus.Server.EntityManagement
 {
+    [DataContract]
     public enum ActivityType
     {
         Unknown,
+        [EnumMember(Value="get")]
         Get,
+        [EnumMember(Value="set")]
         Set,
+        [EnumMember(Value="create")]
         Create,
+        [EnumMember(Value="remove")]
         Remove
     }
 }
