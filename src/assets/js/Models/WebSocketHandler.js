8161893ae8c2e922b48f6583b4f8f39ff65f1f31
Stijn Buurman <s.buurman@student.fontys.nl>
2015-04-20 21:04:50 +0200

    Grunt Taskrunner integration
    -Setup of Gruntfile.js
    -Setup of Package.json
    the following tasks:
        "grunt": "^0.4.5",
        "grunt-contrib-clean": "^0.6.0",
        "grunt-contrib-concat": "^0.5.1",
        "grunt-contrib-copy": "^0.8.0",
        "grunt-contrib-cssmin": "^0.12.2",
        "grunt-contrib-htmlmin": "^0.4.0",
        "grunt-contrib-jshint": "^0.11.2",
        "grunt-contrib-uglify": "^0.9.1",
        "grunt-contrib-watch": "^0.6.1",
        "grunt-includes": "^0.5.1",
        "grunt-uncss": "^0.4.2"
    -Clean up of js thanks to jshint
    -removal of Font Awesome (replaced with bootstrap’s glyph icons)

--- src/assets/js/Models/WebSocketHandler.js
+++ src/assets/js/Models/WebSocketHandler.js
@@ -1,15 +1,15 @@
 function WebSocketHandler(console) {
     this.con = console;
-    this.WebSocketC;
+    this.WebSocketC = null;
     //create some data
-    this.Zones = new ZoneModel([new zone(new school(), new roadc()), new zone(new school, new roadc())]);
+    this.Zones = new ZoneModel([new zone(new school(), new roadc()), new zone(new school(), new roadc())]);
     ko.applyBindings(this.Zones);
 
 
     this.send = function (message) {
         this.con.appendFromClient(message);
         this.WebSocketC.ws.send(message);
-    }
+    };
     this.onOpen = function () {
         //Hide connected and show disconnect button
         $(".connect").hide();
@@ -29,26 +29,27 @@ function WebSocketHandler(console) {
     this.onMessage = function (e) {
         var message = e.data;
         this.con.appendFromServer(message);
+        var json;
         try {
-            var json = JSON.parse(message);
-        } catch (e) {
+            json = JSON.parse(message);
+        } catch (ev) {
             console.log("this is no json");
         }
         //if ports
         var self = this;
-        if (json['payloadtype'] == "ports") {
+        if (json.payloadtype == "ports") {
             self.Zones.availableArduinoPorts.removeAll();
-            $.each(json['payload'], function (key) {
+            $.each(json.payload, function (key) {
                 self.Zones.availableArduinoPorts.push(this);
             });
-        } else if (json['payloadtype'] == "temperature") {
+        } else if (json.payloadtype == "temperature") {
             var minT = -30;
             var maxT = 50;
-            var tempT = json['payload'][0] + -(minT);
+            var tempT = json.payload[0] + -(minT);
             var percentT = tempT / (maxT + -(minT)) * 100;
             $(".temp").css("width", percenT + "%");
         }
-        dl("message type:" + json['payloadtype'] + ", Message: " + json['payload']);
+        dl("message type:" + json.payloadtype + ", Message: " + json.payload);
     };
     this.onClose = function () {
         $(".connect").show();
@@ -63,41 +64,39 @@ function WebSocketHandler(console) {
     this.onDisconnect = function () {
         this.send(">DISC:;");
         this.Zones.removeAll();
-    }
+    };
     this.addZone = function (newZone) {
-        if (typeof newZone.X == "undefined") {
-            var x = "0";
-        } else {
+        var x = "0";
+        if (typeof newZone.X != "undefined") {
             x = newZone.X;
         }
-        if (typeof newZone.Y == "undefined") {
-            var y = "0";
-        } else {
-            var y = newZone.Y;
+        var y = "0";
+        if (typeof newZone.Y != "undefined") {
+            y = newZone.Y;
         }
         this.send(">Create:zone:" + newZone.ID + ":" + x + ":" + y + ":;");
-    }
+    };
     this.removeZone = function (id) {
         this.send(">Remove:zone:" + id + ":;");
-    }
+    };
 
     this.addSchool = function (zoneID, school) {
         var dateStart = school.DateStart.replace(':', '-');
         var dateEnd = school.DateEnd.replace(':', '-');
         this.send(">Create:school:" + zoneID + ":" + school.ID + ":" + school.Name + ":" + dateStart + ":" + dateEnd + ":;");
-    }
+    };
     this.removeSchool = function (schoolId) {
         this.send(">Remove:school:" + schoolId + ":;");
-    }
+    };
     this.addRoadC = function (zoneID, roadc) {
         this.send(">Create:roadconstruction:" + zoneID + ":" + roadc.ID + ":" + roadc.Name + ":" + roadc.DateStart + ":" + roadc.DateEnd + ":;");
-    }
+    };
     this.removeRoadC = function (roadConstructionId) {
         this.send(">remove:roadconstruction:" + roadConstructionId + ":;");
-    }
+    };
     this.connectArduino = function (zoneID, portName) {
         this.send(">SET:zone:" + zoneID + ":road:" + portName + ":;");
-    }
+    };
     this.getData = function () {
         //Ask for all Zones
         dl('Get All Zones');
@@ -119,7 +118,7 @@ function WebSocketHandler(console) {
 
             dl('Get Edges for Zone:' + this.Zones.ZoneList[key].ID);
             this.send(">GET:edges:" + this.Zones.ZoneList[key].ID + ":;");
-        };
+        }
 
 
     };
--- src/assets/js/settings.js
+++ src/assets/js/settings.js
@@ -1,3 +1,3 @@
 var Settings = {
     URI: "ws://localhost:42424"
-}
+};
