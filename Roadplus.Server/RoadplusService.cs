a2f8f11e2891721ed612d77704f1bbeb3c8895a8
Thomas Schraven <t.schraven@student.fontys.nl>
2015-04-20 19:10:40 +0200

    restored some old functionality

---
+++ Roadplus.Server/RoadplusService.cs
@@ -0,0 +1,117 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using Roadplus.Server.Communication.Protocol;
+using Roadplus.Server.Communication;
+using Roadplus.Server.EntityManagement;
+using Roadplus.Server.Traffic;
+
+namespace Roadplus.Server
+{
+    public class RoadplusService
+    {
+        private ActivityFactory activityFactory;
+
+        private WSSessionManager websocketService;
+        private RoadLinkManager roadLinkService;
+        private HttpService httpService;
+
+        private List<Channel> channels;
+        private List<EntityManager> entityManagers;
+
+        private string fileRoot;
+
+        public RoadplusService(Settings settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException("settings");
+            }
+
+            channels = new List<Channel>();
+            entityManagers = new List<EntityManager>();
+            fileRoot = settings.FileRoot;
+
+            if (settings.EnableHttp)
+            {
+                httpService = new HttpService(
+                    new IPEndPoint(
+                    settings.IP,
+                    settings.HttpPort),
+                    settings.HttpRoot);
+            }
+
+            IPEndPoint wsendpoint = new IPEndPoint(
+                settings.IP,
+                settings.Port);
+
+            MessageExchange messageExchange = new MessageExchange();
+
+            activityFactory = new ActivityFactory(messageExchange);
+            activityFactory.Register(new JSONFormat());
+            activityFactory.Register(new PlainTextFormat());
+
+            websocketService = new WSSessionManager(messageExchange,
+                                                    wsendpoint);
+            activityFactory.Register(websocketService);
+            channels.Add(websocketService);
+
+            roadLinkService = new RoadLinkManager(messageExchange,
+                                                  settings.BaudRate);
+            activityFactory.Register(roadLinkService);
+            channels.Add(roadLinkService);
+        }
+
+        public void Start()
+        {
+            Trace.WriteLine(
+                "Starting RoadPlus service...");
+
+            if (httpService != null)
+            {
+                httpService.Start();
+            }
+
+            foreach (Channel c in channels)
+            {
+                c.Start();
+            }
+
+            foreach (EntityManager e in entityManagers)
+            {
+                //e.Load();
+            }
+
+            Trace.WriteLine(
+                "... started");
+        }
+
+        public void Stop()
+        {
+            Trace.WriteLine(
+                "Stopping RoadPlus service...");
+
+            if (httpService != null)
+            {
+                httpService.Stop();
+            }
+
+            foreach (Channel c in channels)
+            {
+                c.Stop();
+            }
+
+            foreach (EntityManager e in entityManagers)
+            {
+                // e.Save();
+            }
+
+            Trace.WriteLine(
+                "... stopped");
+        }
+    }
+}
+
---
+++ Roadplus.Server/Settings.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+
+namespace Roadplus.Server
+{
+    public class Settings
+    {
+        public IPAddress IP { get; set; }
+        public int Port { get; set; }
+        public bool EnableHttp { get; set; }
+        public bool LogToFile { get; set; }
+        public int HttpPort { get; set; }
+        public int BaudRate { get; set; }
+        public string FileRoot { get; set; }
+        public string HttpRoot { get; set; }
+
+        public Settings()
+        {
+            IP = IPAddress.Any;
+            Port = 42424;
+            EnableHttp = true;
+            LogToFile = false;
+            HttpPort = 8080;
+            BaudRate = 38400;
+            FileRoot = Path.GetDirectoryName(
+                Assembly.GetExecutingAssembly().Location);
+            HttpRoot = Path.Combine(FileRoot, "www/");
+        }
+    }
+}
+
