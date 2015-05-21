2d3ad5f12bf46a65ed9415673a04bf96bbd2d367
Stijn Buurman <s.buurman@student.fontys.nl>
2015-05-21 12:55:43 +0200

    Revert "Added Linux Build"
    This reverts commit a3da97372084b5ea6f604647747b1704acb725d5.

--- Gruntfile.js
+++ Gruntfile.js
@@ -160,7 +160,7 @@ module.exports = function (grunt) {
         },
         nodewebkit: {
             options: {
-                platforms: ['win', 'osx','linux'],
+                platforms: ['win', 'osx'],
                 buildDir: './webkitbuilds', // Where the build version of my node-webkit app is saved 
                 version: '0.12.1'
             },
