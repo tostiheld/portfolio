54b5180e50c510b5a13fdf125e854fdaddab581b
Thomas Schraven <t.schraven@student.fontys.nl>
2015-05-30 19:17:32 +0200

    fixed build script syntax error

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Build script for roadplus-embedded.
+# Build script for roadplus-embedded on a travis-ci environment.
 # Add new projects to this script to keep the ci environment
 # running.
 
@@ -11,9 +11,11 @@
 # Example build command:
 # cd <project-name> && ano build -s . -d ../arduino-1.6.1 && cd ..
 
+echo "This script is meant to be run on travis-ci"
+echo "Building with ano version:"
 ano version
 
 # project build commands be below here
 
 # test project
-cd blink && && ano build -d ../arduino-1.6.1 && cd ..
+cd blink && ano build -d ../arduino-1.6.1 && cd ..
