70705f8a5c4b4cb64db38f1609563e1aa473789f
Thomas Schraven <t.schraven@student.fontys.nl>
2015-05-30 17:18:29 +0200

    cleanup new server branch

--- Road/Arduino_Latest/RoadPlus_Arduino/TemperatureSensor.ino
+++
@@ -1,9 +0,0 @@
-void SendTemperature()
-{
-  sensors.requestTemperatures();
-  double temperature = sensors.getTempCByIndex(0);
-  String tempMessage = ">TEMP:" + (String)temperature + ":;";
-  Serial.println(tempMessage);
-  meta = "";
-}
-
--- Road/RoadPlusArduino/TemperatureSensor.ino
+++
@@ -1,8 +0,0 @@
-void SendTemperature()
-{
-  sensors.requestTemperatures();
-  double temperature = sensors.getTempCByIndex(0);
-  String tempMessage = ">TEMP:" + (String)temperature + ":;";
-  Serial.println(tempMessage);
-}
-
