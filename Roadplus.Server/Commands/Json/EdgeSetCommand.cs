e77fcb628e7cee5a5618d8149706ffcd1bb2c8a1
Thomas Schraven <t.schraven@student.fontys.nl>
2015-06-23 13:57:35 +0200

    edge set command

--- Roadplus.Server/Commands/Json/EdgeSetCommand.cs
+++ Roadplus.Server/Commands/Json/EdgeSetCommand.cs
@@ -1,11 +1,85 @@
 ﻿using System;
+using Roadplus.Server.API;
+using Roadplus.Server.Data;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using LinqToDB;
 
 namespace Roadplus.Server.Commands.Json
 {
-    public class EdgeSetCommand
+    public class EdgeSetCommand : ICommand
     {
-        public EdgeSetCommand()
+        private const string ZoneIdKey = "zoneId";
+        private const string startVertexXKey = "startVertexX";
+        private const string startVertexYKey = "startVertexY";
+        private const string endVertexXKey = "endVertexX";
+        private const string endVertexYKey = "endVertexY";
+
+        public string Name { get { return "createEdgeSet"; } }
+
+        public IResponse Execute(string payload)
         {
+            JObject o = JsonConvert.DeserializeObject<JObject>(payload);
+
+            int zoneId, startVertexX, startVertexY, endVertexX, endVertexY;
+
+            if (!Int32.TryParse(o[ZoneIdKey].ToString(), out zoneId) ||
+                !Int32.TryParse(o[startVertexXKey].ToString(), out startVertexX) ||
+                !Int32.TryParse(o[startVertexYKey].ToString(), out startVertexY) ||
+                !Int32.TryParse(o[endVertexXKey].ToString(), out endVertexX) ||
+                !Int32.TryParse(o[endVertexYKey].ToString(), out endVertexY))
+            {
+                return new ResponseFailure(
+                    Name,
+                    "Int32 Parse error");
+            }
+
+            Vertex startVertex = new Vertex()
+            {
+                    ZoneId = zoneId,
+                    X = startVertexX,
+                    Y = startVertexY
+            };
+            
+            Vertex endVertex = new Vertex()
+            {
+                    ZoneId = zoneId,
+                    X = endVertexX,
+                    Y = endVertexY
+            };
+
+            RoadplusData data = new RoadplusData();
+
+            int startVertexId = Convert.ToInt32(
+                data.InsertWithIdentity<Vertex>(startVertex));
+
+            int endVertexId = Convert.ToInt32(
+                data.InsertWithIdentity<Vertex>(endVertex));
+
+            startVertex.VertexId = startVertexId;
+            endVertex.VertexId = endVertexId;
+
+            Edge newEdge = new Edge()
+            {
+                    ZoneId = zoneId,
+                    StartVertexId = startVertexId,
+                    EndVertexId = endVertexId
+            };
+
+            int edgeId = Convert.ToInt32(
+                data.InsertWithIdentity<Edge>(newEdge));
+
+            newEdge.EdgeId = edgeId;
+
+            return new EdgeSetResponse()
+            {
+                ZoneId = zoneId,
+                CreatedEdge = newEdge,
+                StartVertex = startVertex,
+                EndVertex = endVertex
+            };
         }
     }
 }
--- Roadplus.Server/Commands/Json/EdgeSetResponse.cs
+++ Roadplus.Server/Commands/Json/EdgeSetResponse.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 
+using Newtonsoft.Json;
+
 using Roadplus.Server.API;
+using Roadplus.Server.Data;
 
 namespace Roadplus.Server.Commands.Json
 {
-    public class EdgeSetResponse
+    [JsonObject]
+    public class EdgeSetResponse : IResponse
     {
-        public EdgeSetResponse()
-        {
-        }
+        [JsonProperty("response")]
+        public string ResponseString { get { return "OK"; } }
+
+        [JsonProperty("command")]
+        public string Command { get { return "createEdgeSet"; } set { } }
+
+        [JsonProperty("zoneId")]
+        public int ZoneId { get; set; }
+
+        [JsonProperty("createdEdge")]
+        public Edge CreatedEdge { get; set; }
+
+        [JsonProperty("startVertex")]
+        public Vertex StartVertex { get; set; }
+
+        [JsonProperty("endVertex")]
+        public Vertex EndVertex { get; set; }
     }
 }
 
